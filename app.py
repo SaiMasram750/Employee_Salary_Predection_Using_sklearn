571dfe5d96480b6094f825c80d755b1df0944c90
SaiMasram750 <saimasram54@gmail.com>
2025-07-21 17:55:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import joblib
 
 # Load the trained model
-model = joblib.load("best_model.pkl")
+model = joblib.load("./best_model.pkl")
 
 st.set_page_config(page_title="Employee Salary Classification", page_icon="💼", layout="centered")
 st.title("💼 Employee Salary Classification App")
